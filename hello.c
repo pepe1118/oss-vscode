9a47083d4487434e00833b5945cb63dd598998e0
Open Source SW <opensw-geek@gmail.com>
2025-11-19 16:01:10 +0900

    Changed: hello.c(M), README.md(A)

--- hello.c
+++ hello.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
 void main() {
-    printf("Complete Installing VSCode..!\n");
+  printf("------------- hello.c -------------\n");
+  printf("Hi, It's my first repository created by VSCode.\n");
 }
